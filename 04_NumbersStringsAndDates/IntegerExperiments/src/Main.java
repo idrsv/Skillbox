d718a57f2159da5a5df0f067249f0574018af878
Данил <idrisovds@gmail.com>
2020-03-08 00:19:34 +0300

    1-Bug fix 2-Implementation of sumDigits method

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -2,14 +2,31 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Container container = new Container();
-        container.count += 7843;
-
+        System.out.println(sumDigits(256));
     }
 
-    public Integer sumDigits(Integer number)
+    public static int sumDigits(int number)
     {
-        //@TODO: write code here
-        return 0;
+        String line = String.valueOf(number);
+
+        int sum = 0;
+        for (int i = 0; i < line.length(); i++)
+        {
+            sum += Character.getNumericValue(line.charAt(i));
+        }
+        return sum;
     }
+
+    public static int sumDigits(Integer number)
+    {
+        String line = Integer.toString(number);
+
+        int sum = 0;
+        for (int i = 0; i < line.length(); i++)
+        {
+            sum += Integer.parseInt(String.valueOf(line.charAt(i)));
+        }
+        return sum;
+    }
+
 }
