1776eccc4b57bb1eb391750ec20798d837afed4d
Данил <idrisovds@gmail.com>
2020-03-09 14:31:27 +0300

    Returned the line and fixed the problem in the class Container

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -2,6 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Container container = new Container();
+        container.count += 7843;
+
         System.out.println(sumDigits(256));
     }
 
