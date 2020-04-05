2bdff493347831471e3b0019854c24bb43e046de
master <idrisovds@gmail.com>
2020-04-05 22:10:14 +0300

    1-wrote character numbers for the latin alphabet 2-the code considers the amount

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,10 +1,14 @@
-
 public class Loader
 {
     public static void main(String[] args)
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-
         System.out.println(text);
+
+        Integer vasyaSum = Integer.parseInt(text.substring(15, text.indexOf("руб")).trim());
+        Integer mashaSum = Integer.parseInt(text.substring(text.indexOf("Маша - ") + 6, text.lastIndexOf("руб")).trim());
+
+        int sum = vasyaSum + mashaSum;
+        System.out.println(sum);
     }
 }
