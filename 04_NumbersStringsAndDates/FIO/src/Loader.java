459bdc8cc297e02c04c82c7ff1335bd2e6272927
master <idrisovds@gmail.com>
2020-04-08 14:56:29 +0300

    Task completed Full Name with *

--- 04_NumbersStringsAndDates/FIO/src/Loader.java
+++ 04_NumbersStringsAndDates/FIO/src/Loader.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
+public class Loader {
+    public static void main(String[] args) {
         System.out.println("Введите: Фамилию Имя Отчество");
         Scanner scanner = new Scanner(System.in);
         String fullName = scanner.nextLine();
 
         int firstIndex = fullName.indexOf(' ');
         int lastIndex = fullName.lastIndexOf(' ');
+        int space = 0;
 
-        String firstName = fullName.substring(0, firstIndex);
-        String lastName = fullName.substring(firstIndex + 1, lastIndex);
-        String middleName = fullName.substring(lastIndex + 1);
-
-        System.out.println("Фамилия: " + firstName);
-        System.out.println("Имя: " + lastName);
-        System.out.println("Отчество: " + middleName);
+        boolean fullName2 = true;
+        for (int i = 0; i < fullName.length(); i++) {
+            if (Character.isWhitespace(fullName.charAt(i))) {
+                space++;
+            }
+            if ((fullName.charAt(i) >= '0' && fullName.charAt(i) <= '9') || (space < 2 | space > 2)) {
+                fullName2 = false;
+            }
+        }
+        if (fullName2 == false) {
+            System.out.println("Вы допустили ошибку при вводе данных");
+        }
+            else {
+                String firstName = fullName.substring(0, firstIndex);
+                String lastName = fullName.substring(firstIndex + 1, lastIndex);
+                String middleName = fullName.substring(lastIndex + 1);
+                System.out.println("Фамилия: " + firstName);
+                System.out.println("Имя: " + lastName);
+                System.out.println("Отчество: " + middleName);
+            }
     }
 }
