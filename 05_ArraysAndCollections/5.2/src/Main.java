694e11fb7d4df53a7d6e380e20125f6e40e60d0e
Danil <idrisovds@gmail.com>
2020-08-08 16:18:42 +0500

    Learned to work with the ArrayList

--- 05_ArraysAndCollections/5.2/src/Main.java
+++ 05_ArraysAndCollections/5.2/src/Main.java
@@ -1,57 +1,65 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    public static final ArrayList<String> todoList = new ArrayList<>() {{
+        add(0, "Сходить в магазин");
+        add(1, "Сходить на тренировку");
+        add(2, "Сделать домашнюю работу на Skillbox.ru");
+        add(3, "Отнести посылку на почту");
+    }};
     public static void main(String[] args) {
-        int numberNewDeal;
-        String newDeal;
-        int dealNumber;
-        String descriptionOfTheEditedCase;
-         ArrayList<String> todoList = new ArrayList<>() {{
-            add(0, "Сходить в магазин");
-            add(1, "Сходить на тренировку");
-            add(2, "Сделать домашнюю работу на Skillbox.ru");
-            add(3, "Отнести посылку на почту");
-        }};
+
+        System.out.println("\n-------------------------------- \nВведите команду: \nADD \nEDIT \nDELETE \nLIST \n--------------------------------");
         while (true) {
-            System.out.println("\n-------------------------------- \nВведите нужный номер команды: \n1 - ADD \n2 - EDIT \n3 - DELETE \n4 - LIST \n--------------------------------");
             Scanner scanner = new Scanner(System.in);
-            String deals = scanner.nextLine();
-            switch (deals) {
-                case "1":
-                    System.out.println("Введите порядковый номер нового дела");
-                    Scanner scanner1 = new Scanner(System.in);
-                    numberNewDeal = scanner1.nextInt() - 1;
-                    System.out.println("Введите описание нового дела");
-                    Scanner scanner2 = new Scanner(System.in);
-                    newDeal = scanner2.nextLine();
-                    todoList.add(numberNewDeal, newDeal);
-                    System.out.println("Выполнено!");
-                    continue;
-                case "2":
-                    System.out.println("Введите порядковый номер дела из списка, который вы хотите редактировать");
-                    Scanner scanner3 = new Scanner(System.in);
-                    dealNumber = scanner3.nextInt() - 1;
-                    System.out.println("Введите новое описание дела ");
-                    Scanner scanner4 = new Scanner(System.in);
-                    descriptionOfTheEditedCase = scanner4.nextLine();
-                    todoList.set(dealNumber, descriptionOfTheEditedCase );
-                    System.out.println("Выполнено!");
-                    continue;
-                case "3":
-                    System.out.println("Введите порядковый номер дела, которое необходимо удалить и списка");
-                    todoList.remove(scanner.nextInt() - 1);
-                    System.out.println("Выполнено!");
-                    continue;
-                case "4":
-                    System.out.println("Ваш список дел: ");
-                    for (int i = 0; i < todoList.size(); i++)
-                    {
-                        System.out.println((i + 1) + " " + todoList.get(i));
-                    };
+            String input = scanner.nextLine() + " ";
+
+            String command = input.substring(0,input.indexOf(" ")).toUpperCase().trim();
+            String numberAndDescription = input.substring(command.length()).trim();
+            String number = numberAndDescription.replaceAll("[А-Яа-я]|[A-Za-z]\\w]+","");
+            String description = numberAndDescription.replaceAll("[0-9]+","");
+
+            switch (command) {
+                case "ADD":
+                    if (number.equals("")){
+                        todoList.add(description);
+                    }
+                    else if (Integer.parseInt(number) > todoList.size()){
+                        todoList.add(description);
+                    }
+                    else {
+                        todoList.add(Integer.parseInt(number),description);
+                    }
                     break;
+                case "EDIT":
+                    if (Integer.parseInt(number) < todoList.size()){
+                        todoList.set(Integer.parseInt(number),description);}
+                    else {
+                        System.out.println("Такого дела в вашем списке нет");
+                    }
+                    break;
+                case "DELETE":
+                    if (Integer.parseInt(number) < todoList.size()){
+                        todoList.remove(Integer.parseInt(number));}
+                    else {
+                        System.out.println("Такого дела в вашем списке нет");
+                    }
+                    break;
+                case "LIST":
+                    System.out.println("Ваш список дел: ");
+                    for (int i = 0; i < todoList.size(); i++) {
+                        System.out.println((i + 1) + " " + todoList.get(i));
+                    }
+                    break;
+                default:
+                    System.out.println("Ошибка!\nВведите команду правильно");
             }
         }
-
     }
 }
+
+
