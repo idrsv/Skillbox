d97a88e8f793e1cc04f585ce176f22df2f72a950
Danil <idrisovds@gmail.com>
2020-08-08 16:21:44 +0500

    Get experience with arrays. Completed task 1 and 2

--- 05_ArraysAndCollections/5.1/src/Main.java
+++ 05_ArraysAndCollections/5.1/src/Main.java
@@ -1,39 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        //Код, который меняет порядок расположения элементов внутри массива на обратный.
-//        String text = "Каждый охотник желает знать, где сидит фазан";
-//
-//        String[] words = text.split(",?\\s+");
-//
-//        for(int i = words.length - 1; i >= 0; i--)
-//        {
-//            System.out.println(words[i]);
-//        }
-        //Код, который выводит количество пациентов, их максимальную и минимальную температуру и так же для здоровых пациентов.
-//        float[] patientsTemperature = new float[30];
-//        float min = 32;
-//        float max = 40;
-//        float minHealthyPatients = 36.2f;
-//        float maxHealthyPatients = 36.9f;
-//        float resultTemperaturePatients = 0;
-//        float resultTemperatureHealthyPatients = 0;
-//        int amountHealthyPatients = 0;
-//
-//        for (int i = 1; patientsTemperature.length > i; i++) {
-//            float value = (float) Math.random() * (max - min) + min;
-//            patientsTemperature[i] = value;
-//            System.out.println("Пациент " + i + " с температурой " + String.format("%.1f", value).replace(',', '.'));
-//            resultTemperaturePatients += value;
-//            if (minHealthyPatients <= value && value <= maxHealthyPatients) {
-//                amountHealthyPatients++;
-//                resultTemperatureHealthyPatients += value;
-//            }
-//        }
-//        resultTemperaturePatients = resultTemperaturePatients/30;
-//        System.out.println("Средняя температура по больнице " + String.format("%.1f", resultTemperaturePatients).replace(',', '.'));
-//        resultTemperatureHealthyPatients = resultTemperatureHealthyPatients/amountHealthyPatients;
-//        System.out.println("Средняя температура здоровых пациентов по больнице " + String.format("%.1f", resultTemperatureHealthyPatients).replace(',', '.'));
-//        System.out.println("Количество здоровых пациентов " + amountHealthyPatients);
-//    }
-        //
+        Код, который меняет порядок расположения элементов внутри массива на обратный.
+        String text = "Каждый охотник желает знать, где сидит фазан";
+
+        String[] words = text.split(",?\\s+");
+
+        for(int i = words.length - 1; i >= 0; i--)
+        {
+            System.out.println(words[i]);
+        }
+        Код, который выводит количество пациентов, их максимальную и минимальную температуру и так же для здоровых пациентов.
+        float[] patientsTemperature = new float[30];
+        float min = 32;
+        float max = 40;
+        float minHealthyPatients = 36.2f;
+        float maxHealthyPatients = 36.9f;
+        float resultTemperaturePatients = 0;
+        float resultTemperatureHealthyPatients = 0;
+        int amountHealthyPatients = 0;
+
+        for (int i = 1; patientsTemperature.length > i; i++) {
+            float value = (float) Math.random() * (max - min) + min;
+            patientsTemperature[i] = value;
+            System.out.println("Пациент " + i + " с температурой " + String.format("%.1f", value).replace(',', '.'));
+            resultTemperaturePatients += value;
+            if (minHealthyPatients <= value && value <= maxHealthyPatients) {
+                amountHealthyPatients++;
+                resultTemperatureHealthyPatients += value;
+            }
+        }
+        resultTemperaturePatients = resultTemperaturePatients/30;
+        System.out.println("Средняя температура по больнице " + String.format("%.1f", resultTemperaturePatients).replace(',', '.'));
+        resultTemperatureHealthyPatients = resultTemperatureHealthyPatients/amountHealthyPatients;
+        System.out.println("Средняя температура здоровых пациентов по больнице " + String.format("%.1f", resultTemperatureHealthyPatients).replace(',', '.'));
+        System.out.println("Количество здоровых пациентов " + amountHealthyPatients);
+    }
 }
