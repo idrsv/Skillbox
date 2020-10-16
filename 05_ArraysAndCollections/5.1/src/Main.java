484df65bf93a611b162f1b9a2b23af552b1342a5
Danil <idrisovds@gmail.com>
2020-10-16 20:37:15 +0300

    Complete task

--- 05_ArraysAndCollections/5.1/src/Main.java
+++ 05_ArraysAndCollections/5.1/src/Main.java
@@ -1,38 +1,53 @@
+import java.util.Arrays;
 public class Main {
+    static double min = 32;
+    static double max = 40;
+    static double minHealthyPatients = 36.2;
+    static double maxHealthyPatients = 36.9;
+    static double value;
+
     public static void main(String[] args) {
-        Код, который меняет порядок расположения элементов внутри массива на обратный.
+//// Код, который меняет порядок расположения элементов внутри массива на обратный.
         String text = "Каждый охотник желает знать, где сидит фазан";
+        String[] words1 = text.split(",?\\s+");
+        String words2;
+        for (int i=0; i < words1.length; i++) {
+            System.out.println(words1[i]); }
+        int a = words1.length;
 
-        String[] words = text.split(",?\\s+");
-
-        for(int i = words.length - 1; i >= 0; i--)
+        for(int i = 0; i < a / 2; i++)
         {
-            System.out.println(words[i]);
-        }
-        Код, который выводит количество пациентов, их максимальную и минимальную температуру и так же для здоровых пациентов.
-        float[] patientsTemperature = new float[30];
-        float min = 32;
-        float max = 40;
-        float minHealthyPatients = 36.2f;
-        float maxHealthyPatients = 36.9f;
-        float resultTemperaturePatients = 0;
-        float resultTemperatureHealthyPatients = 0;
-        int amountHealthyPatients = 0;
+            words2 = words1[a - i - 1];
+            words1[a - i - 1] = words1[i];
+            words1[i] = words2; }
+        for (int i=0; i < words1.length; i++) {
+            System.out.println(words1[i]); }
 
-        for (int i = 1; patientsTemperature.length > i; i++) {
-            float value = (float) Math.random() * (max - min) + min;
-            patientsTemperature[i] = value;
+// Код, который выводит количество пациентов, их максимальную и минимальную температуру и так же для здоровых пациентов.
+        double[] temperaturePatients = new double[30];
+        for (int i = 0; temperaturePatients.length > i; i++) {
+            value =  (Math.random() * (max - min) + min);
+            temperaturePatients[i] = value;
             System.out.println("Пациент " + i + " с температурой " + String.format("%.1f", value).replace(',', '.'));
-            resultTemperaturePatients += value;
-            if (minHealthyPatients <= value && value <= maxHealthyPatients) {
-                amountHealthyPatients++;
-                resultTemperatureHealthyPatients += value;
-            }
         }
-        resultTemperaturePatients = resultTemperaturePatients/30;
-        System.out.println("Средняя температура по больнице " + String.format("%.1f", resultTemperaturePatients).replace(',', '.'));
-        resultTemperatureHealthyPatients = resultTemperatureHealthyPatients/amountHealthyPatients;
-        System.out.println("Средняя температура здоровых пациентов по больнице " + String.format("%.1f", resultTemperatureHealthyPatients).replace(',', '.'));
-        System.out.println("Количество здоровых пациентов " + amountHealthyPatients);
+        System.out.println("Средняя температура по больнице " +  String.format("%.1f", averageTemperature(temperaturePatients)).replace(',', '.'));
+        System.out.println("Количество здоровых пациентов " + getHealthyPatientsCount(temperaturePatients));
+    }
+
+    public static double averageTemperature(double[] temperaturePatients) {
+        double averageTemp = 0;
+        for (int i = 0; temperaturePatients.length > i; i++) {
+            averageTemp += value; }
+        averageTemp = averageTemp/temperaturePatients.length;
+        return averageTemp;
+    }
+
+    public static int getHealthyPatientsCount(double[] temperaturePatients) {
+        int healthyCount = 0;
+        for (int i = 0; temperaturePatients.length > i; i++) {
+        if (minHealthyPatients <= temperaturePatients[i] && temperaturePatients[i] <= maxHealthyPatients)
+            healthyCount++;}
+        return healthyCount;
     }
 }
+
