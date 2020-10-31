1ab88bcd23d1f851cd54ea82aea6245369da2084
Danil <idrisovds@gmail.com>
2020-10-31 19:15:10 +0300

    Made corrections in hw 5.4

--- 05_ArraysAndCollections/5.4/src/Main.java
+++ 05_ArraysAndCollections/5.4/src/Main.java
@@ -1,37 +1,35 @@
 import java.util.*;
 public class Main {
-
+    private static TreeMap<String, String> phoneBook = new TreeMap<>();
     public static void main(String[] args) {
-
-        HashMap<String, String> phoneBook = new HashMap<>();
         // Map <K, V> Данил 89853685585
-        for(;;){
-
-            Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        for (; ; ) {
             String input = scanner.nextLine();
             String nameOrNumber = input.trim();
-            String subscribersName = nameOrNumber.replaceAll("([\\d]+)","").trim();
-            String subscribersPhoneNumber = nameOrNumber.replaceAll("([А-Яа-я]+)","").trim();
+            String subscribersName = nameOrNumber.replaceAll("([\\d]+)", "").trim();
+            String subscribersPhoneNumber = nameOrNumber.replaceAll("([А-Яа-я]+)", "").trim();
 
-            if(nameOrNumber.equals("LIST")){
+            if (nameOrNumber.equals("LIST")) {
                 printPhoneBook(phoneBook);
                 continue;
             }
-            if (phoneBook.containsKey(subscribersName)){
-                System.out.println("Абонент - " + subscribersName + " с номером " +  phoneBook.get(subscribersName));
-            }
-            else if (phoneBook.containsValue(subscribersPhoneNumber)){
-                System.out.println("По номеру " + subscribersPhoneNumber + " зарегистрирован абонент - " + phoneBook.get(subscribersPhoneNumber));
-            }
-            else phoneBook.put(subscribersName,subscribersPhoneNumber);
+            if (phoneBook.containsKey(subscribersName)) {
+                System.out.println("Абонент - " + subscribersName + " с номером " + phoneBook.get(subscribersName));
+            } else if (phoneBook.containsValue(subscribersPhoneNumber)) {
+                for (Map.Entry<String, String> entry : phoneBook.entrySet()){
+                    if (entry.getValue().equals(subscribersPhoneNumber)){
+                        System.out.println("По номеру " + subscribersPhoneNumber + " зарегистрирован абонент - " + entry.getKey());
+                    }
+                }
+            } else phoneBook.put(subscribersName, subscribersPhoneNumber);
         }
     }
 
-   private static void printPhoneBook(HashMap<String, String> phoneBook){
+    private static void printPhoneBook(Map<String, String> map) {
         System.out.println("Телефонный справочник: ");
-        for(Map.Entry<String,String> k: phoneBook.entrySet()){
-            System.out.println(k.getKey()+" - "+ k.getValue());
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
         }
-   }
+    }
 }
