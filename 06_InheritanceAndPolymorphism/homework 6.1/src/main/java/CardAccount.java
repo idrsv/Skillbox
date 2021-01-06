bf42bec7e68ffcc59397ad12edd778b48089ffe5
master <idrisovds@gmail.com>
2021-01-06 19:22:59 +0500

    Fixed mistakes in homework 6.1

--- 06_InheritanceAndPolymorphism/homework 6.1/src/main/java/CardAccount.java
+++
@@ -1,9 +0,0 @@
-public class CardAccount extends BankAccount {
-    // Реализация класса карточного счета
-    @Override
-    public void take(double amountToTake) {
-        double percentToTake = amountToTake * 0.01;
-        super.take(amountToTake + percentToTake);
-        System.out.println("Выполнено!");
-    }
-}
