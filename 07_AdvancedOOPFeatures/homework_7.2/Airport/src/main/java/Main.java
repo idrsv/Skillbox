b795d61115c94e28c2c6afcea31bdc2fcede779d
Danil <idrisovds@gmail.com>
2021-01-30 19:55:09 +0300

    Fixed 7.2.2

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        findPlanesLeavingInTheNextTwoHours(Airport.getInstance());
+        List<Flight> flights = findPlanesLeavingInTheNextTwoHours(Airport.getInstance());
+        flights.forEach(System.out::println);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
