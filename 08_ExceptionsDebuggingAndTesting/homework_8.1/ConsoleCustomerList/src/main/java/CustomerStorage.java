aff7fcea84cd5234a12f55f55007f2b1712a8feb
Danil <idrisovds@gmail.com>
2021-02-03 22:41:29 +0300

    Wrote options for protecting against incorrect data and premature completion of the addCustomer () method.

--- 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
@@ -13,10 +15,25 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
-
-        String[] components = data.split("\\s+");
-        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+            String[] components = data.split("\\s+");
+            if (components.length != 4){
+                throw new IllegalArgumentException("Wrong format. Correct format \n" +
+                        "add Василий Петров vasily.petrov@gmail.com +79215637722");
+            }
+            String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+            Pattern VALID_EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+            Pattern VALID_PHONE_REGEX = Pattern.compile("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$");
+            Matcher matcherEmail = VALID_EMAIL_REGEX.matcher(components[INDEX_EMAIL]);
+            Matcher matcherPhone = VALID_PHONE_REGEX.matcher(components[INDEX_PHONE]);
+            if (!matcherEmail.find()){
+                throw new IllegalArgumentException("Wrong format. Correct format email \n" +
+                    "vasily.petrov@gmail.com");
+            }
+            if (!matcherPhone.find()){
+                throw new IllegalArgumentException("Wrong format. Correct format phone number \n" +
+                        "+79215637722");
+            }
+            storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
     public void listCustomers() {
--- 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/Main.java
+++ 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/Main.java
@@ -12,23 +12,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
+        for(;;) {
+            try {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
 
-        while (true) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                }
+            }
+            catch (IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
             }
         }
     }
