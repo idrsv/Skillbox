cdf7e4e822cc7413c7b2d9a06c5e6fa5f34345cc
Danil <idrisovds@gmail.com>
2021-02-15 20:00:20 +0300

    Finished hw 9.1/Created classes Main and FileUtils, implemented static method calculateFolderSize. All tests are performed.

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,20 @@
-public class FileUtils {
+import java.io.File;
 
+public class FileUtils {
     public static long calculateFolderSize(String path) {
-        return 0;
+        File source = new File(path);
+        long filesSize = 0L;
+        try {
+            if (source.isDirectory()){
+                File[] newFile = source.listFiles();
+                for (File file : newFile){
+                    filesSize += calculateFolderSize(file.getAbsolutePath());
+                }
+            }
+            else filesSize = source.length();
+        } catch (NullPointerException ex){
+            ex.getStackTrace();
+        }
+        return filesSize;
     }
 }
