4ccf4ec08620be9b3df4ac980b24be6cb6deb200
idrisovds@gmail.com <dGCvkabb97>
2021-03-03 21:21:18 +0300

    first trial hw 9.3

--- 09_FilesAndNetwork/homework_9.3/src/main/java/Movements.java
+++ 09_FilesAndNetwork/homework_9.3/src/main/java/Movements.java
@@ -1,13 +1,70 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movements {
+    public static List<Double> movementIncome;
+    public static List<Double> movementExpense;
 
     public Movements(String pathMovementsCsv) {
+        Movements.getIncome(pathMovementsCsv);
+        Movements.getExpense(pathMovementsCsv);
+    }
+
+    private static List<Double> getExpense(String pathMovementsCsv){
+        movementExpense = new ArrayList<>();
+        try{
+            List<String> lines = Files.readAllLines(Paths.get(pathMovementsCsv));
+            for (String line : lines) {
+                String[] fragments = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                if (fragments.length != 8) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                if (!line.startsWith("Тип")){
+                    movementExpense.add(Double.parseDouble(fragments[7].replace(",",".").replace("\"","")));
+                }
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return movementExpense;
+    }
+
+    private static List<Double> getIncome(String pathMovementsCsv){
+        movementIncome = new ArrayList<>();
+        try{
+            List<String> lines = Files.readAllLines(Paths.get(pathMovementsCsv));
+            for (String line : lines) {
+                String[] fragments = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                if (fragments.length != 8) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                if (!line.startsWith("Тип")) {
+                    movementIncome.add(Double.parseDouble(fragments[6].replace(",", ".").replace("\"", "")));
+                }
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return movementIncome;
     }
 
     public double getExpenseSum() {
-        return 0.0;
+        double expenseSum = 0.0;
+        for (Double aDouble : movementExpense) {
+            expenseSum += aDouble;
+        }
+        return expenseSum;
     }
 
     public double getIncomeSum() {
-        return 0.0;
+        double incomeSum = 0.0;
+        for (Double aDouble : movementIncome){
+            incomeSum += aDouble;
+        }
+        return incomeSum;
     }
 }
