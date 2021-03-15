5b1c2b54a233b1784614decd12fd4269bb33a168
023ksit <idrisovds@gmail.com>
2021-03-15 20:39:20 +0300

    add classes Station,Lina and Metro

---
+++ 09_FilesAndNetwork/homework_9.5/src/main/java/core/Station.java
@@ -0,0 +1,35 @@
+package core;
+
+public class Station {
+    private String lineID;
+    private String nameStation;
+
+    public Station(String lineID, String nameStation) {
+        this.lineID = lineID;
+        this.nameStation = nameStation;
+    }
+
+    public String getLineID() {
+        return lineID;
+    }
+
+    public void setLineID(String lineID) {
+        this.lineID = lineID;
+    }
+
+    public String getNameStation() {
+        return nameStation;
+    }
+
+    public void setNameStation(String nameStation) {
+        this.nameStation = nameStation;
+    }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+                "lineID='" + lineID + '\'' +
+                ", nameStation='" + nameStation + '\'' +
+                '}';
+    }
+}
