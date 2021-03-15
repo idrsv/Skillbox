89937007c6758677855789d9442faaa6a0dcb6d1
023ksit <idrisovds@gmail.com>
2021-03-15 20:37:54 +0300

    second trial h.w 9.5

--- 09_FilesAndNetwork/homework_9.5/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.5/src/main/java/Main.java
@@ -1,66 +1,45 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import core.Line;
+import core.Metro;
+import core.Station;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class Main {
-
     private static final String URL = "https://www.moscowmap.ru/metro.html#lines";
     private static final String PATH = "C:\\Users\\ksit\\java_basics\\09_FilesAndNetwork\\homework_9.5\\src\\main\\resources\\mosMetro.json";
-    private static final Pattern numberOfLine = Pattern.compile("(?<=ln-)\\w+");
-    private static final Pattern nameOfLine = Pattern.compile("(?<=>).+(?=<)");
 
-    private static JSONObject jsonObject = new JSONObject();
-    private static JSONObject lineObject;
-    private static JSONObject stationObject;
-    private static JSONArray linesArray;
-    private static JSONArray stationsArray;
-
-    private static List<String> lineList = new ArrayList<>();
-    private static int countLines;
 
     public static void main(String[] args) {
-        try (FileWriter fileWriter = new FileWriter(PATH)) {
-            addLinesAndStations();
-            fileWriter.write(String.valueOf(jsonObject));
+        try {
+            Document document = Jsoup.connect(URL).maxBodySize(0).get();
+            Elements elementsOfLines = document.select("span.js-metro-line.t-metrostation-list-header.t-icon-metroln");
+            List<Line> listOfLines = new ArrayList<>();
+            Map<String,String> namesOfLines = elementsOfLines.stream()
+                    .collect(Collectors.toMap((k) -> k.attr("data-line"), Element::text));
+            namesOfLines.forEach((k, v) -> listOfLines.add(new Line(k, v)));
+
+            Elements elementsOfStations = document.select("div.js-metro-stations.t-metrostation-list-table").select("span.name");
+
+            Map<String, List<Station>> listOfStations = elementsOfStations.stream();
+
+            Metro metro = new Metro(listOfLines,listOfStations);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            mapper.writeValue(new File(PATH), metro);
+            System.out.println(listOfLines);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void addLinesAndStations() throws IOException {
-        Document document = Jsoup.connect(URL).maxBodySize(0).get();
-        Elements elementsOfLines = document.select("div.js-toggle-depend");
-        Elements elementsOfStations = document.select("span.num, span.name");
-
-        Matcher matcherNumber = numberOfLine.matcher(elementsOfLines.html());
-        Matcher matcherName = nameOfLine.matcher(elementsOfLines.html());
-
-        linesArray = new JSONArray();
-        while (matcherName.find() && matcherNumber.find()) {
-            lineObject = new JSONObject();
-            lineList.add(countLines, matcherNumber.group());
-            lineObject.put("Number", matcherNumber.group());
-            lineObject.put("Name", matcherName.group());
-            linesArray.add(lineObject);
-            countLines++;
-        }
-
-        String[] nameOfStation = elementsOfStations.text().split(" 1\\.");
-        stationObject = new JSONObject();
-        for (int i = 0; i < countLines; i++) {
-            stationsArray = new JSONArray();
-            Collections.addAll(stationsArray, nameOfStation[i].replaceAll("[0-9]+\\.", "").trim().split(" {2}"));
-            stationObject.put(lineList.get(i), stationsArray);
-        }
-
-        jsonObject.put("Line", linesArray);
-        jsonObject.put("Station", stationObject);
     }
 }
