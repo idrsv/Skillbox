22c69572b921f73285ad9526942045d432cb2fb0
023ksit <idrisovds@gmail.com>
2021-03-22 18:28:14 +0300

    finished h.w. 9.5!!

--- 09_FilesAndNetwork/homework_9.5/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.5/src/main/java/Main.java
@@ -1,23 +1,28 @@
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import core.Line;
 import core.Metro;
 import core.Station;
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class Main {
     private static final String URL = "https://www.moscowmap.ru/metro.html#lines";
-    private static final String PATH = "C:\\Users\\ksit\\java_basics\\09_FilesAndNetwork\\homework_9.5\\src\\main\\resources\\mosMetro.json";
+    private static final String PATH = "src/main/resources/mosMetro.json";
 
 
     public static void main(String[] args) {
@@ -32,31 +37,24 @@ public class Main {
 
             Elements elementsOfStations = document.getElementsByClass("js-metro-stations");
             Map<String, List<Station>> listOfStations = new HashMap<>();
+            Map<String, Integer> countStationsOnLine = new HashMap<>();
+            elementsOfStations.forEach(el -> el.children().forEach(element -> {
+                if (!listOfStations.containsKey(el.attr("data-line"))) {
+                    listOfStations.put((el.attr("data-line")), new ArrayList<>());
+                }
+                listOfStations.get(el.attr("data-line")).add(new Station(element.getElementsByClass("name").text()));
+                countStationsOnLine.put((el.attr("data-line")), (listOfStations.get(el.attr("data-line")).size()));
+            }));
+            for(String entry : countStationsOnLine.keySet() ){
+                String key = entry.toString();
+                String value = countStationsOnLine.get(entry).toString();
+                System.out.println("Номер ветки: " + key + "\t Количество станций: " + value);
+            }
 
-            elementsOfStations.forEach(el -> {
-                el.children().forEach(element -> {
-                    if (!listOfStations.containsKey(el.attr("data-line"))) {
-                        listOfStations.put((el.attr("data-line")), new ArrayList<>());
-                    }
-                    listOfStations.get(el.attr("data-line")).add(new Station(element.getElementsByClass("name").text()));
-                });
-            });
-            System.out.println(listOfStations);
             Metro metro = new Metro(listOfLines, listOfStations);
             ObjectMapper mapper = new ObjectMapper();
-
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
             mapper.writeValue(new File(PATH), metro);
-
-            BufferedReader bf = new BufferedReader(new FileReader(PATH));
-            ObjectNode root = (ObjectNode)new ObjectMapper().readTree(bf);
-            int count = 0;
-            for (Iterator<JsonNode> i = root.get("stations").iterator(); i.hasNext();){
-                if( i.next().hasNonNull(""))
-                count++;
-            }
-            System.out.println(count);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
